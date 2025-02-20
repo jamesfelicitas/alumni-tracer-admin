54467e2b7dc0261ce39bc4942bc13da50e67b30d
cliffamadeus <dev.clfm5t12@gmail.com>
2025-02-20 14:41:04 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h3" component="h2">
+        Welcome to Vite + Material UI
+      </Typography>
+      <Button variant="contained" color="primary">
+        Get Started
+      </Button>
+    </div>
+  );
+}
+
+export default App;
