0ae13b6918f67af6487a8f2bd5c1c4bd13e7070d
cliffamadeus <dev.clfm5t12@gmail.com>
2025-04-23 15:56:55 +0800

    Import changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState} from 'react';
 import { Grid, Card, CardContent, Typography, useTheme, IconButton } from '@mui/material';
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
